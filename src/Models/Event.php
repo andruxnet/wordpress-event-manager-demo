4c6307e45c40df1f0e2ec23143348e2d07206fa3
Andres Olvera <aolvera@andrux.net>
2025-08-07 20:01:34 -0600

    Create Event model for representing event data objects
    Bump to PHP 8.1 version in composer.json to support readonly properties.

---
+++ src/Models/Event.php
@@ -0,0 +1,45 @@
+<?php
+
+  namespace Andruxnet\EventManager\Models;
+
+  /**
+   * Event model representing an event post record.
+   */
+  class Event
+  {
+    /**
+     * Create a new Event instance.
+     *
+     * @param int       $id           Event post ID
+     * @param string    $title        Event title
+     * @param string    $description  Event description
+     * @param \DateTime $eventDate    Event date and time
+     * @param string    $location     Event location
+     * @param int       $capacity     Maximum attendees to the event (0 = unlimited)
+     */
+    public function __construct(
+      public readonly int $id,
+      public readonly string $title,
+      public readonly string $description,
+      public readonly \DateTime $eventDate,
+      public readonly string $location,
+      public readonly int $capacity
+    ) {}
+
+    /**
+     * Return event instance as array.
+     *
+     * @return array
+     */
+    public function toArray(): array {
+      return [
+        'id' => $this->id,
+        'title' => $this->title,
+        'description' => $this->description,
+        'event_date' => $this->eventDate,
+        'location' => $this->location,
+        'capacity' => $this->capacity,
+      ];
+    }
+
+  }
