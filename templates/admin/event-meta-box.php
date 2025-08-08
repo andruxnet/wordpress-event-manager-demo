036ecccd40089ef1e27e5704287d54748649152d
Andres Olvera <aolvera@andrux.net>
2025-08-07 22:29:37 -0600

    Apply minor formatting improvements to the event-meta-box template

--- templates/admin/event-meta-box.php
+++ templates/admin/event-meta-box.php
@@ -2,7 +2,9 @@
   <tbody>
     <tr>
       <th scope="row">
-        <label for="event-date"><?php _e('Event Date', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?></label>
+        <label for="event-date">
+          <?php _e('Event Date', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?>
+        </label>
       </th>
       <td>
         <input type="date"
@@ -11,13 +13,17 @@
                value="<?php echo esc_attr($date); ?>"
                class="regular-text"
         />
-        <p class="description"><?php _e('Select the event date', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?></p>
+        <p class="description">
+          <?php _e('Select the event date', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?>
+        </p>
       </td>
     </tr>
 
     <tr>
       <th scope="row">
-        <label for="event-location"><?php _e('Location', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?></label>
+        <label for="event-location">
+          <?php _e('Location', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?>
+        </label>
       </th>
       <td>
         <input type="text"
@@ -27,13 +33,17 @@
                class="regular-text"
                placeholder="<?php esc_attr_e('Enter event location', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?>"
         />
-        <p class="description"><?php _e('Event venue or address', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?></p>
+        <p class="description">
+          <?php _e('Event venue or address', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?>
+        </p>
       </td>
     </tr>
 
     <tr>
       <th scope="row">
-        <label for="event-capacity"><?php _e('Capacity', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?></label>
+        <label for="event-capacity">
+          <?php _e('Capacity', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?>
+        </label>
       </th>
       <td>
         <input type="number"
@@ -41,9 +51,11 @@
                name="event-capacity"
                value="<?php echo esc_attr($capacity); ?>"
                class="small-text"
-               min="1"
+               min="0"
         />
-        <p class="description"><?php _e('Maximum number of attendees (leave empty for unlimited)', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?></p>
+        <p class="description">
+          <?php _e('Maximum number of attendees (set 0 for unlimited)', Andruxnet\EventManager\Plugin::TEXT_DOMAIN); ?>
+        </p>
       </td>
     </tr>
   </tbody>
